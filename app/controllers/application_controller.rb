18bd3caa2b6acf1b2e109503b91f9e8202c8bf9d
Carson Meiling <carmc93@gmail.com>
2020-03-15 20:25:00 -0600

    added first and last name to db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_perameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def configure_permitted_perameters
+    devise_perameter_sanitizer.permit(:sign_up, keys[:first_name, :last_name])
+  end
 end
